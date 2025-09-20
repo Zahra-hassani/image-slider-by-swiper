608ee8d409b32d4b9c786dd99b2722c5f003260c
Zahra-developer <hassanizahra897@gmail.com>
2025-09-20 10:14:39 +0430

    some cards added

--- src/components/Bookstory.jsx
+++ src/components/Bookstory.jsx
@@ -1,7 +1,63 @@
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css";
+import "swiper/css/navigation";
 import Story from "./Story"
+import { Navigation } from "swiper/modules"
 
 export default function Bookstory(){
     const stories = [
+        {
+            name: "Beauty & the beast",
+            imgUrl: "./img/beauty.jpeg",
+        },
+        {
+            name: "Cinderella",
+            imgUrl: "./img/cinderella.jpeg",
+        },
+        {
+            name: "Aurora",
+            imgUrl: "./img/aurora.jpg",
+        },
+        {
+            name: "Princess",
+            imgUrl: "./img/pincess.jpeg",
+        },
+        {
+            name: "Rapanzel",
+            imgUrl: "./img/rapanzel.jpeg",
+        },
+        {
+            name: "Snow White",
+            imgUrl: "./img/snowwhite.jpeg",
+        },
+        {
+            name: "Princess and the frog",
+            imgUrl: "./img/princessfrog.png",
+        },
+        {
+            name: "Brave",
+            imgUrl: "./img/brave.jpeg",
+        },
+        {
+            name: "Mulan",
+            imgUrl: "./img/mulan.jpeg",
+        },
+        {
+            name: "Jasmine",
+            imgUrl: "./img/jasmin2.jpeg",
+        },
+        {
+            name: "Raya",
+            imgUrl: "./img/raya2.jpeg",
+        },
+        {
+            name: "Pocahontas",
+            imgUrl: "./img/pocahontas.jpeg",
+        },
+        {
+            name: "Frozen",
+            imgUrl: "./img/frozen.jpeg",
+        },
         {
             name: "Moral story 1",
             imgUrl: "./img/Lion.jpeg",
@@ -14,16 +70,31 @@ export default function Bookstory(){
             name: "Moral story 3",
             imgUrl: "./img/King.jpeg",
         },
-        {
-            name: "Beauty & the beast",
-            imgUrl: "./img/beauty.jpeg",
-        },
     ]
     return (
-        <div className="h-[40vh] w-full flex justify-between items-center px-3 gap-4 my-4 flex-wrap">
-            {stories.map((item) =>(
-                <Story key={item.name} imgUrl={item.imgUrl} name={item.name} />
-            ))}
+        <div className="h-fit w-full flex justify-center flex-col items-center p-2 ">
+            <h1 className="text-3xl font-bold text-center py-4">The best selling Stories</h1>
+            <p className="text-center text-xl">Start with top stories of the world</p>
+            <div className="h-[40vh] w-full flex justify-between items-center px-3 gap-2 my-4">
+            <Swiper navigation={true} modules={[Navigation]} slidesPerView={[4]} className="w-full flex justify-between gap-3" >
+                <SwiperSlide><Story imgUrl={stories[0].imgUrl} name={stories[0].name} /></SwiperSlide>
+                <SwiperSlide><Story imgUrl={stories[1].imgUrl} name={stories[1].name} /></SwiperSlide>
+                <SwiperSlide><Story imgUrl={stories[2].imgUrl} name={stories[2].name} /></SwiperSlide>
+                <SwiperSlide><Story imgUrl={stories[3].imgUrl} name={stories[3].name} /></SwiperSlide>
+                <SwiperSlide><Story imgUrl={stories[4].imgUrl} name={stories[4].name} /></SwiperSlide>
+                <SwiperSlide><Story imgUrl={stories[5].imgUrl} name={stories[5].name} /></SwiperSlide>
+                <SwiperSlide><Story imgUrl={stories[6].imgUrl} name={stories[6].name} /></SwiperSlide>
+                <SwiperSlide><Story imgUrl={stories[7].imgUrl} name={stories[7].name} /></SwiperSlide>
+                <SwiperSlide><Story imgUrl={stories[8].imgUrl} name={stories[8].name} /></SwiperSlide>
+                <SwiperSlide><Story imgUrl={stories[9].imgUrl} name={stories[9].name} /></SwiperSlide>
+                <SwiperSlide><Story imgUrl={stories[10].imgUrl} name={stories[10].name} /></SwiperSlide>
+                <SwiperSlide><Story imgUrl={stories[11].imgUrl} name={stories[11].name} /></SwiperSlide>
+                <SwiperSlide><Story imgUrl={stories[12].imgUrl} name={stories[12].name} /></SwiperSlide>
+                {/* <SwiperSlide><Story imgUrl={stories[13].imgUrl} name={stories[13].name} /></SwiperSlide> */}
+                {/* <SwiperSlide><Story imgUrl={stories[14].imgUrl} name={stories[14].name} /></SwiperSlide> */}
+                {/* <SwiperSlide><Story imgUrl={stories[15].imgUrl} name={stories[15].name} /></SwiperSlide> */}
+            </Swiper>
+            </div>
         </div>
     )
 }
--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,8 +1,8 @@
 export default function Story({imgUrl,name}){
     return (
-        <div className="w-[15%] h-59">
+        <div className="w-[55%] h-64">
         <div className="h-full w-full rounded flex flex-col gap-3 shadow-md">
-            <img src={imgUrl} className="rounded-t h-[70%] w-full" alt="" />
+            <img src={imgUrl} className="rounded-t h-[80%] w-full" alt="" />
             <h1 className="font-bold text-2xl text-center">{name}</h1>
         </div>
         </div>
